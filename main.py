380f2902d9b390933835c989636515086f5af1ac
artsli <arthursliwa@icloud.com>
2020-04-25 17:07:17 +0200

    Args 2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 import time
 import sys
+import getopt
 
 class Bot:
-    def __init__(self):
+    def __init__(self, hash, xpath, browser):
         self.driver = webdriver.Firefox(executable_path='/home/ego/Downloads/geckodriver-v0.26.0-linux64/geckodriver')
 
     def closeBrowser(self):
@@ -17,10 +18,34 @@ class Bot:
         click_elem.click()
 
 # hash # xpath # times
-if __name__ == "main":
 
-    for i in range(1, sys.argv[2]+1):
-        BOT = Bot()
+
+def main():
+
+    try:
+        options, args = getopt.getopt(
+            sys.argv[1:], "h:x:b:p:v",
+            ["hash=", "xpath=", "browser=", "path=", "votes="])
+        for name, value in options:
+            if name in ('-h', '--hash'):
+                HASH = value
+            if name in ('-x', '--xpath'):
+                XPATH = value
+            if name in ('-b', '--browser'):
+                BROWSER = value
+            if name in ('-p', '--path'):
+                PATH = value
+            if name in ('-v', '--votes'):
+                VOTES = value
+
+    except getopt.GetoptError as err:
+        print(str(err))
+        print("Invalid args!")
+        sys.exit(1)
+    for i in range(1, sys.argv[2] + 1):
+        BOT = Bot(HASH, XPATH, BROWSER)
         BOT.vote()
         BOT.closeBrowser()
         print("Votes: " + str(i))
+if __name__ == "main":
+    main()
